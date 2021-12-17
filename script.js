e07746ca3ad35e3387988e0883655a484709a810
Tulio <tuliopalacine@hotmail.com>
2021-12-16 23:50:42 -0300

    final

---
+++ script.js
@@ -0,0 +1,15 @@
+const mobileBtn = document.getElementById('mobile-cta')
+let nav = document.querySelector('nav')
+let mobileBtnExit = document.getElementById('mobile-exit')
+
+function clicarOpen(){
+    mobileBtn.addEventListener
+    nav.classList.add('menu-btn')
+}
+
+function clicarClose(){
+    mobileBtnExit.addEventListener
+    nav.classList.remove('menu-btn')
+}
+
+
